507b631acfcd373bbd06265c0c879619623a5507
U-DESKTOP-TKKJJAU\gaming <bcwik9@gmail.com>
2020-07-24 18:34:15 -0400

    update zillow to use table

--- zillow_favorite_enhancer.js
+++ zillow_favorite_enhancer.js
@@ -7,10 +7,12 @@
 // @author      Ben Cwik
 // @description 7/23/2020, 5:15:54 PM
 // @require http://code.jquery.com/jquery-3.3.1.min.js
+// @require http://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js
 // ==/UserScript==
 
 $(function(){
   var data = JSON.parse($("#__NEXT_DATA__").text())['props']['apolloInitialCacheData'];
+  var main_body = $('div[class^="Favorites__SavedHomesWrapper"]');
   var card_list = $("div[class^='FavoritesList__ListCardsWrapper']");
   var menu_wrap = $('div[class^="FavoritesList__MenuBarWrapper"]');
   var card = $(card_list.children()[0]);
@@ -20,60 +22,100 @@ $(function(){
   var pending_listing_regex = /(contingent|accepting.backups|pending|under.contract)/i;
   var off_market_listing_regex = /(sold|off.market|other|recently.sold)/i;
   
-  var zillow_page_loaded = function(){
-    console.log("ZILLOW PAGE LOADED");
+  var init_table = function(){
+    // load datatables CSS
+    $('head').append('<link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" type="text/css" />');
     
-    // add new select filter
-    var existing_select = menu_wrap.find('select')[0];
-    var existing_label = menu_wrap.find('label')[0];
-    menu_wrap.append(select_html);
-    var custom_filter = menu_wrap.find('select#custom-filter');
-    var custom_label = menu_wrap.find('label#custom-label');
-    custom_filter.addClass(existing_select.className);
-    custom_label.addClass(existing_label.className);
-    custom_filter.change(custom_filter_changed);
-  };
-  
-  var custom_filter_changed = function(){
-    // hide unrelated elements
-    hide_other_elements();
+    // add table to dom
+    var info_html = '<br><h2 style="text-align:center;">Saved Homes</h2>';
+    var table_html = '<table id="custom-table" class="display compact" style="width:100%;text-align:center;"><thead><tr>'+
+        '<th>Picture</th>'+
+        '<th>Price</th>'+
+        '<th>Status</th>'+
+        '<th>Bed</th>'+
+        '<th>Bath</th>'+
+        '<th>Sqft</th>'+
+        '<th>City</th>'+
+        '<th>State</th>'+
+        '<th>Zip</th>'+
+        '<th>Type</th>'+
+        '<th>Built</th>'+
+        '<th>Heating</th>'+
+        '<th>Cooling</th>'+
+        '<th>Parking</th>'+
+        '<th>HOA</th>'+
+        '<th>Lot</th>'+
+        '<th>Price/sqft</th>'+
+        '<th>Broker</th>'+
+        '<th>Saves</th>'+
+        '<th>Days on Zillow</th>'+
+        '</tr></thead>'+
+        '<tbody></tbody>'+
+        '</table>';
     
-    // append cards that match filter to result list
-    var selected_val = $(event.target.selectedOptions[0]).val();
-    var listings = eval(selected_val)();
+    main_body.html(
+      '<div style="margin-left:100px;margin-right:100px;">'+
+      info_html+
+      table_html+
+      '</div>'
+    );
+    var table = $('#custom-table');
+    var body = table.find('tbody');
+    
+    // populate the table with row data
+    var listings = all_listings();
     $.each(listings, function(i,listing_id){
-      append_card(listing_id);
+      var data = data_for_property(listing_id);
+      var interval = setInterval(function(){
+        if(data['picture_url'] && data['facts']){
+          clearInterval(interval);
+          var parking_match = get_fact(data['facts'], 'Parking').match(/(\d+)/);
+          var parking = parking_match ? parking_match[1] : '';
+          var days_on_fact = get_fact(data['facts'], 'Days on Zillow');
+          var days_on_match = days_on_fact ? days_on_fact.match(/(\d+)/) : undefined;
+          var days_on_zillow = days_on_match ? days_on_match[1] : '';
+          var row = '<tr>'+
+              '<td><a href="'+ data['url'] +'">'+'<img src="'+ data['picture_url'] +'" style="max-width:200px;"></a></td>'+
+              '<td>'+ data['price'] + '</td>'+
+              '<td>'+ data['status'] + '</td>'+
+              '<td>'+ data['bedrooms'] + '</td>'+
+              '<td>'+ data['bathrooms'] + '</td>'+
+              '<td>'+ data['living_area'] + '</td>'+
+              '<td>'+ data['city'] + '</td>'+
+              '<td>'+ data['state'] + '</td>'+
+              '<td>'+ data['zipcode'] + '</td>'+
+              '<td>'+ get_fact(data['facts'], 'Type') + '</td>'+
+              '<td>'+ get_fact(data['facts'], 'Year Built') + '</td>'+
+              '<td>'+ get_fact(data['facts'], 'Heating') + '</td>'+
+              '<td>'+ get_fact(data['facts'], 'Cooling') + '</td>'+
+              '<td>'+ parking + '</td>'+
+              '<td>'+ get_fact(data['facts'], 'HOA') + '</td>'+
+              '<td>'+ get_fact(data['facts'], 'Lot') + '</td>'+
+              '<td>'+ get_fact(data['facts'], 'Price/sqft') + '</td>'+
+              '<td>'+ data['brokerage'] + '</td>'+
+              '<td>'+ get_fact(data['facts'], 'Saves') + '</td>'+
+              '<td>'+ days_on_zillow + '</td>'+
+              '</tr>';
+          body.append(row);
+        }
+      }, 100);
     });
     
-    var menu_bar = $(menu_wrap.children()[0]);
-    menu_bar.html('<h3>'+ listings.length + ' Listing(s) Shown</h3>');
-    menu_bar.css('background','none');
-    menu_bar.css('border','none');
+    // wait for data to be populated before initializing DataTables
+    var data_ready = setInterval(function(){
+      if(body.find('tr').length == listings.length){
+        clearInterval(data_ready);
+        table.DataTable({
+          "lengthMenu": [[-1, 10, 25, 50], ['All', 10, 25, 50]],
+          "order": [[1, "asc" ]]
+        });
+      }
+    }, 250);
+    
   }
   
-  var hide_other_elements = function(){
-    // hide navigation for pagination
-    var nav_pagination = card_list.siblings("nav");
-    nav_pagination.hide();
-    
-    // clear existing results list
-    card_list.html('');
-  }
-  
-  var append_card = function(data_index){
-    // append a placeholder card
-    card_list.append(card_html);
-    var new_card = card_list.children().last();
-    var card_info = new_card.find('.list-card-info');
-    var zpid, address;
-    
-    // hide the share button. TODO: get button to work?
-    new_card.find('button:contains("Share")').hide();
-
-    // hide save button. TODO: get button to work?
-    new_card.find('button.list-card-save').hide();
-    
-    // replace the placeholder data with the actual data
+  var data_for_property = function(data_index){
+    var property_data = {};
     var regex = new RegExp('\\.' + data_index + '(\\.|$)');
     $.each(data, function(key,val){
       if(key.match(regex)){
@@ -81,52 +123,35 @@ $(function(){
         //console.log(val);
         //console.log('---------------------------')
         if(key.match(/property$/)){
-          // status. TODO: match status icon with actual status (maybe just change color?)
-          var status_wrap = card_info.find('.list-card-type');
+          property_data['status'] = titleize(val['homeStatus']);
           var status_class= 'zsg-icon-for-sale';
           if(val['homeStatus'].match(off_market_listing_regex)){
             status_class = 'zsg-icon-off-market';
           }
-          var status_icon = '<span class="list-card-type-icon '+ status_class +'"></span>';
-          status_wrap.html(status_icon + titleize(val['homeStatus']));
-          // price
-          var price = card_info.find('.list-card-price');
-          price.text(val['countryCurrency'] + val['price']);
-          // details
-          var details = card_info.find('.list-card-details');
-          var bedrooms = $(details.find('li')[0]);
-          var bedroom_label = bedrooms.find('abbr').prop('outerHTML');
-          bedrooms.html(val['bedrooms'] + bedroom_label);
-          var bathrooms = $(details.find('li')[1]);
-          var bathroom_label = bathrooms.find('abbr').prop('outerHTML');
-          bathrooms.html(val['bathrooms'] + bathroom_label);
-          var living_area = $(details.find('li')[2]);
-          var living_area_label = living_area.find('abbr').prop('outerHTML');
-          living_area.html(val['livingArea'] + living_area_label);
-          // URL link
-          var link_url = new_card.find('.list-card-link');
-          link_url.prop('href', val['hdpUrl']);
-          // ZPID
-          zpid = val['zpid'];
+          property_data['status_icon'] = '<span class="list-card-type-icon '+ status_class +'"></span>';
+          
+          property_data['price'] = val['countryCurrency'] + val['price'];
+          property_data['bedrooms'] = val['bedrooms'];
+          property_data['bathrooms'] = val['bathrooms'];
+          property_data['living_area'] = val['livingArea'];
+          property_data['url'] = val['hdpUrl'];
+          property_data['zpid'] = val['zpid'];
         }
         if(key.match(/address$/)){
-          var address_wrap = card_info.find('.list-card-addr');
-          address = val['streetAddress']+', '+val['city']+', '+val['state']+ ' ' + val['zipcode'];
-          address_wrap.text(address);
+          property_data['street_address'] = val['streetAddress'];
+          property_data['city'] = val['city'];
+          property_data['state'] = val['state'];
+          property_data['zipcode'] = val['zipcode'];
+          property_data['full_address'] = val['streetAddress']+', '+val['city']+', '+val['state']+ ' ' + val['zipcode'];;
         }
         if(key.match(/postingContact$/)){
-          var brokerage = new_card.find('.list-card-brokerage .list-card-truncate');
-          brokerage.text(val['brokerageName']);
+          property_data['brokerage'] = val['brokerageName'];
         }
       }
     });
-    
-    // replace zpid
-    var article = new_card.find('article.list-card');
-    article.attr('id', 'zpid_'+zpid);
-    
+     
     // fetch additional info not provided on the page already
-    fetch_home_details(zpid).then(
+    fetch_home_details(property_data['zpid']).then(
       function(response) {
         // check for errors
         if (response.status !== 200) {
@@ -137,19 +162,7 @@ $(function(){
 
         // process response and replace property facts
         response.json().then(function(data) {
-          var facts = data['data']['property']['homeFacts']['atAGlanceFacts'];
-          var detail_list = new_card.find('ul[data-loading="false"]');
-          $.each(detail_list.children('li'), function(i,e){
-            var label = $(e).find('label');
-            var value = label.siblings();          
-            var new_value = get_fact(facts, label.text())
-            if(new_value){
-              value.text(new_value);            
-            } else {
-              console.log("Couldn't replace fact: " + label.text() + ". Deleting element.");
-              $(e).remove();
-            }
-          })
+          property_data['facts'] = data['data']['property']['homeFacts']['atAGlanceFacts'];
         });
       }
     )
@@ -158,7 +171,7 @@ $(function(){
     })
     
     // fetch pictures
-    fetch_property_pictures(zpid).then(
+    fetch_property_pictures(property_data['zpid']).then(
       function(response) {
         // check for errors
         if (response.status !== 200) {
@@ -169,16 +182,15 @@ $(function(){
 
         // process response and replace property facts
         response.json().then(function(data) {
-          var new_pic_url = data['data']['property']['photos'][0]['url'];
-          var img = new_card.find('.list-card-link img');
-          img.prop('src', new_pic_url);
-          img.attr('alt', address);
+          property_data['picture_url'] = data['data']['property']['photos'][0]['url'];
         });
       }
     )
     .catch(function(err) {
       console.log('Picture Fetch Error :-S', err);
-    })    
+    })
+    
+    return property_data;
   }
   
   var fetch_home_details = function(zpid){
@@ -216,16 +228,12 @@ $(function(){
     });
   }
   
-  var select_html = '<br><label id="custom-label">Filter:</label>'+
-      '<select id="custom-filter">'+
-      '<option></option>'+
-      '<option value="active_listings">Active Listings</option>'+
-      '<option value="pending_listings">Pending Listings</option>'+
-      '<option value="off_market_listings">Off Market Listings</option>'+
-      '</select>';
-  
   var titleize = function(str) {
     return str.replaceAll('_', ' ');
+  }
+  
+  var all_listings = function(){
+    return find_listings(/./);
   }
   
   var active_listings = function(){
@@ -248,25 +256,16 @@ $(function(){
         matched.push(match[1]);
       }
     });
-    return matched;    
+    return matched;
   }
   
   var init = function(){
-    // poll until zillow page is loaded, then save existing card HTML
-    var loaded = false;
-    var interval = setInterval(function(){
-      var loaded_info_cards = card_list.find('div[data-loading="false"]');
-      $.each(loaded_info_cards, function(i,card){
-        // find a card with all 8 details (ie. lot size, sqft, type, date built, etc)
-        if(!loaded && $(card).find('li').length >= 7){
-          clearInterval(interval);
-          loaded = true;
-          card_html = $(loaded_info_cards[0]).parent().prop('outerHTML');
-          zillow_page_loaded();
-        }
-      });
-    }, 250);
-  };
+    var load_button_html = '<br><button id="load-table">Load Table View</button>';
+    menu_wrap.append(load_button_html);
+    var load_button = menu_wrap.find('#load-table')
+    load_button.addClass($("button:contains('Share')")[0].className);
+    load_button.click(init_table);
+  }
   
   init();
 });
